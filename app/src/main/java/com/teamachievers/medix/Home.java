356be109a595f69aaf2e10c03320e234bb74b205
Saurav singh <sauravsingh85079@gmail.com>
2021-10-10 02:10:24 +0530

    Add Home and Nav files

--- app/src/main/java/com/teamachievers/medix/Home.java
+++ app/src/main/java/com/teamachievers/medix/Home.java
@@ -3,24 +3,95 @@ package com.teamachievers.medix;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Locale;
 
 
-public class Home extends Fragment {
+public class Home extends Fragment implements View.OnClickListener {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-     
-    }
+TextView t_Surgeon,t_Psychiatrist,t_Neurologist,t_Dentist,t_General,t_Cardiologist;
+ImageView i_Surgeon,i_Psychiatrist,i_Neurologist,i_Dentist,i_General,i_Cardiologist;
+
+    Bundle bundle = new Bundle();
+
+    private FirebaseFirestore db;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.frag_home, container, false);
+        View v = inflater.inflate(R.layout.frag_home, container, false);
+
+        v.findViewById(R.id.card_General).setOnClickListener(this);
+        t_General = v.findViewById(R.id.card_General).findViewById(R.id.cate_name);
+        t_General.setText("General");
+        i_General =v.findViewById(R.id.card_General).findViewById(R.id.cate_img);
+        i_General.setImageResource(R.drawable.general);
+
+        v.findViewById(R.id.card_General).setOnClickListener(this);
+        t_Surgeon = v.findViewById(R.id.card_Surgeon).findViewById(R.id.cate_name);
+        t_Surgeon.setText("Surgeon");
+        i_Surgeon =v.findViewById(R.id.card_Surgeon).findViewById(R.id.cate_img);
+        i_Surgeon.setImageResource(R.drawable.surgeon);
+
+        v.findViewById(R.id.card_General).setOnClickListener(this);
+        t_Neurologist = v.findViewById(R.id.card_Neurologist).findViewById(R.id.cate_name);
+        t_Neurologist.setText("Neurologist");
+        i_Neurologist =v.findViewById(R.id.card_Neurologist).findViewById(R.id.cate_img);
+        i_Neurologist.setImageResource(R.drawable.neurologist);
+
+        v.findViewById(R.id.card_General).setOnClickListener(this);
+        t_Psychiatrist = v.findViewById(R.id.card_Psychiatrist).findViewById(R.id.cate_name);
+        t_Psychiatrist.setText("Psychiatrist");
+        i_Psychiatrist =v.findViewById(R.id.card_Psychiatrist).findViewById(R.id.cate_img);
+        i_Psychiatrist.setImageResource(R.drawable.psychiatrist);
+
+        v.findViewById(R.id.card_General).setOnClickListener(this);
+        t_Dentist = v.findViewById(R.id.card_Dentist).findViewById(R.id.cate_name);
+        t_Dentist.setText("Dentist");
+        i_Dentist =v.findViewById(R.id.card_Dentist).findViewById(R.id.cate_img);
+        i_Dentist.setImageResource(R.drawable.dentist);
+
+        v.findViewById(R.id.card_General).setOnClickListener(this);
+        t_Cardiologist = v.findViewById(R.id.card_General).findViewById(R.id.cate_name);
+        t_Cardiologist.setText("Cardiologist");
+        i_Cardiologist =v.findViewById(R.id.card_General).findViewById(R.id.cate_img);
+        i_Cardiologist.setImageResource(R.drawable.cardiologist);
+
+        return v;
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Fragment fragment = new Clinics();
+        switch (v.getId()){
+            case R.id.card_General:
+                bundle.putString("cate", "General");
+                bundle.putString("cate_link", "Gen");
+                break;
+            case R.id.card_Dentist:
+                bundle.putString("cate", "Dentist");
+                bundle.putString("cate_link", "Den");
+                break;
+        }
+        fragment.setArguments(bundle);
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameContainer2, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
+
     }
 }
--- app/src/main/java/com/teamachievers/medix/MainActivity2.java
+++ app/src/main/java/com/teamachievers/medix/MainActivity2.java
@@ -1,24 +1,79 @@
 package com.teamachievers.medix;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.PrimitiveIterator;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    private static BottomNavigationView navView;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            return false;
+            Fragment fragment;
+            fragment = new Home();
+            switch (item.getItemId()) {
+                case R.id.navigation_calender:
+                    fragment = new schedule();
+                    break;
+                case R.id.navigation_home:
+                    fragment = new Home();
+                    break;
+
+                case R.id.navigation_profile:
+                    fragment = new Profile();
+                    break;
+            }
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            for (int i = 0; i < fragmentManager.getBackStackEntryCount(); ++i) {
+                fragmentManager.popBackStack();
+            }
+            fragmentTransaction.replace(R.id.frameContainer2, fragment);
+            fragmentTransaction.commit();
+
+            return true;
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        Fragment fragment;
-        fragment = new Clinics();
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frameContainer2, fragment);
-        fragmentTransaction.commit();
+        navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navView.setSelectedItemId(R.id.navigation_home);
+
+    }
+    public void hideBottomNav() {
+        navView.setVisibility(View.GONE);
+    }
+
+    public void showBottomNav() {
+        navView.setVisibility(View.VISIBLE);
+    }
+
+    public void portrait() {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+
+    public void landscape() {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
 }
